6a7f05e2c36ffd4a6e4a851c6b428a1521840ffb
kfhwang <kfhwang@nutc.edu.tw>
2023-12-08 16:03:39 +0800

    20231208

--- GameOfLive/live.js
+++ GameOfLive/live.js
@@ -20,6 +20,7 @@ class Life{
 
     // }
 }
+
 Life.prototype.statusAt = function(row, col){
     if(row<0 || col<0 || row>=this.row || col>=this.col)
       return DEAD;
@@ -38,13 +39,85 @@ Life.prototype.calcNeighbor = function(row, col){
     count += this.statusAt(row+1, col+1)//right bottom
     return count;
 }
+Life.prototype.update = function(){
+    //copy of grid for next generation
+    //var nextGrid=this.grid; //same memory
+    var nextGrid = JSON.parse(JSON.stringify(this.grid));
+    // var kid= {age:5, hight:170}
+    // var kid1 = kid
+    var count=0;
+    for (let row = 0; row < this.row; row++) {
+        for (let col = 0; col < this.col; col++) {
+          count = this.calcNeighbor(row, col);
+          //update LIVE=>DEAD
+          if(this.statusAt(row,col) == LIVE && (count<2 || count>3)){
+            nextGrid[row][col] = DEAD;
+          }
+          //update DEAD=> LIVE 
+          if(this.statusAt(row,col) == DEAD && count==3){
+            nextGrid[row][col] = LIVE;
+          }
+        }
+    }
 
+    //update this.grid
+    this.grid = nextGrid;
+    //gc() //garbage collection
+}
 
+//draw
+class DrawGame{
+    constructor(_game, _canvas){
+        this.game = _game;
+        this.canvas = document.getElementById(_canvas).getContext("2d");
+        var size1 = document.getElementById(_canvas).width/this.game.col;
+        var size2 = document.getElementById(_canvas).height/this.game.row;
+        this.size = Math.min(size1,size2);
+        this.canvas.lineWidth = 1;
+        this.canvas.lineStyle = "#000";
+    }
+}
 
-var game1 = new Life(3,3);
+DrawGame.prototype.draw= function(){
+    for (let row = 0; row < this.game.row; row++) {
+        for (let col = 0; col < this.game.col; col++) {
+           if(this.game.grid[row][col]==LIVE){
+               this.canvas.fillStyle = "#f00";
+           }else{
+                this.canvas.fillStyle = "#fff";
+           }   
+            this.canvas.fillRect(col*this.size, row*this.size, this.size, this.size);
+            this.canvas.strokeRect(col*this.size, row*this.size, this.size, this.size);
+        }
+    }
+}
+
+var game1 = new Life(5,5);
 game1.grid[1][0]=LIVE;
 game1.grid[1][1]=LIVE;
 game1.grid[1][2]=LIVE;
 console.log("(1,1):"+game1.calcNeighbor(1,1));
 console.log("(2,0):"+game1.calcNeighbor(2,0));
+
+var drawgame1 = new DrawGame(game1, "board");
+drawgame1.draw();
+
+function next(){
+    game1.update();
+    //draw
+    drawgame1.draw();
+}
+
+// console.log(game1.grid)
+
 //var game2 = new Life(30,30);
+
+function boardClick(event){
+    var row = Math.floor(event.offsetY/drawgame1.size);
+    var col = Math.floor(event.offsetX/drawgame1.size);
+    if(drawgame1.game.grid[row][col]==LIVE)
+        drawgame1.game.grid[row][col]=DEAD;
+    else
+        drawgame1.game.grid[row][col]=LIVE;
+    drawgame1.draw();
+}
