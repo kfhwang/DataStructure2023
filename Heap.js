6b0a3555811aecd91b9db805d269887978860d70
kfhwang <kfhwang@nutc.edu.tw>
2023-12-29 16:18:43 +0800

    20231229

---
+++ Heap.js
@@ -0,0 +1,43 @@
+class Heap{
+    constructor(_ary){
+        this.ary=_ary;
+    }
+}
+Heap.prototype.sort = function(){
+  //construction heap
+   var length = this.ary.length;
+   for (let internal =parseInt(length/2)-1; internal >= 0; internal--){
+       this.shiftDown(internal, length-1);
+   }
+  //sort
+
+}
+
+
+Heap.prototype.shiftDown = function(internal, end){
+      while(internal*2+1<=end){
+        var left = internal*2+1;
+        var right = internal*2+2;
+        var maxChild = left;
+        if(right<=end && this.ary[right]>this.ary[left]){
+            maxChild = right;
+        }
+        if(this.ary[internal] < this.ary[maxChild]){
+            //swap
+            var temp = this.ary[internal];
+            this.ary[internal] = this.ary[maxChild];
+            this.ary[maxChild] = temp;
+            internal = maxChild;
+        }else{
+            return;
+        }
+
+      }
+}
+
+var myHeap = new Heap([78,8,29,74,58,11,68,10,2,73,50,65,92]);
+myHeap.sort();
+console.log(myHeap.ary)
+
+// var ary=[78,8,29,74,58,11,68,10,2,73,50,65,92]
+// ary.heapSort()
